a64ca679e5941a2e706ea7c8cd842e55c1f67591
Stuming <1361046649@qq.com>
2018-04-08 09:48:54 -0400

    *ENH*change unit of size based on its magnitude

--- scripts/foldersize.py
+++ scripts/foldersize.py
@@ -15,7 +15,15 @@ def folder_size(folder):
         for singlefile in files:
             totalsize = totalsize + os.path.getsize(os.path.join(path, singlefile))
             filenumber = filenumber + 1
-    print('{0}:\n\tsize: {1:.2f}MB\n\tfiles: {2}'.format(folder, totalsize/(1024*1024.0), filenumber))
+
+    print('0{}:'.format(folder))
+    if totalsize/1024.0 < 0.01:
+        print('\tsize: {0}B'.format(totalsize))
+    elif totalsize/(1024*1024.0) < 0.01:
+        print('\tsize: {0:.2f}KB'.format(totalsize/1024.0))
+    else:
+        print('\tsize: {0:.2f}MB'.format(totalsize/(1024*1024.0)))
+    print('\tfiles: {0}'.format(filenumber))
     return totalsize, filenumber
 
 
