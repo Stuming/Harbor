570099d049a4fe17e4921aa6fee7c609072397ab
Stuming <1361046649@qq.com>
2018-04-08 10:02:01 -0400

    *ENH*check if file not exists, which may causes error

--- scripts/foldersize.py
+++ scripts/foldersize.py
@@ -9,20 +9,27 @@ def folder_size(folder):
     """
     totalsize = 0
     filenumber = 0
-    print("Path: {}".format(folder))
 
     for (path, dirs, files) in os.walk(folder):
         for singlefile in files:
-            totalsize = totalsize + os.path.getsize(os.path.join(path, singlefile))
+            singlepath = os.path.join(path, singlefile)
+            if not os.path.exists(singlepath):
+                # prevent invalid softlink caused error
+                print('File not exists: {}'.format(singlepath))
+                continue
+            totalsize = totalsize + os.path.getsize(singlepath)
             filenumber = filenumber + 1
 
-    print('0{}:'.format(folder))
+    print('--'*10)
+    print('{0}:'.format(folder))
     if totalsize/1024.0 < 0.01:
         print('\tsize: {0}B'.format(totalsize))
     elif totalsize/(1024*1024.0) < 0.01:
         print('\tsize: {0:.2f}KB'.format(totalsize/1024.0))
-    else:
+    elif totalsize/(1024*1024*1024.0) < 0.01:
         print('\tsize: {0:.2f}MB'.format(totalsize/(1024*1024.0)))
+    else:
+        print('\tsize: {0:.2f}GB'.format(totalsize/(1024*1024*1024.0)))
     print('\tfiles: {0}'.format(filenumber))
     return totalsize, filenumber
 
